d19ad61afb153f47291505b07c8806c0cd4f65ee
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-06-06 00:59:20 -0300

    Adding style

--- src/components/suscription-table/suscription-table.tsx
+++ src/components/suscription-table/suscription-table.tsx
@@ -27,7 +27,7 @@ export class SuscriptionTable {
 
   co() {
     const TwoCoInlineCart = window['TwoCoInlineCart']
-    TwoCoInlineCart.setup.setMerchant('250139456036'); // meter en variables de entorno
+    TwoCoInlineCart.setup.setMerchant('250163802451'); // meter en variables de entorno
 
     TwoCoInlineCart.products.add({
       code: 'standardM', // codigo de producto
@@ -86,60 +86,95 @@ export class SuscriptionTable {
         <button onClick={() => this.togglingBillingMonthly(true)}>Monthly</button>
         <button onClick={() => this.togglingBillingMonthly(false)}>Annualy</button>
       </div>
-      <div class='flex-1  text-gray-700 text-center  p-2 m-1 rounded-lg shadow-md'>
-        Features
-        <p class='text-left'>Price</p>
-        <p class='text-left'>Feature 1</p>
-        <p class='text-left'>Feature 2</p>
-        <p class='text-left'>Feature 3</p>
-        <p class='text-left'>Feature 4</p>
-        <p class='text-left'>Feature 5</p>
-        <p class='text-left'>Feature 6</p>
-        <p class='text-left'>Feature 7</p>
-        <p class='text-left'>Feature 8</p>
-        <p class='text-left'>Feature 9</p>
+      <div class='flex-1  text-gray-700 text-center box1'>
+        <p class='text-4xl plan_name pt-4  pt-4'>Free</p>
+        <div class='price text-4xl  pt-4'> $0</div>
+        <div class="border-b  pt-4 pb-1"> por mes</div>
+        <div class="border-b pt-4 pb-1"> <p>Periodo de pruebas</p><p  class='uppercase pt-1 font-bold'>Ilimitado</p></div>
+        <div  class="border-b pt-4 pb-1" > Mensajes/Mes <span  class='uppercase pt-1 font-bold'> 500 </span></div>
+        <div class="border-b pt-4 pb-1"> Chatbot Widgets <p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1"> Watson Assistant (IBM)  </div>
+        <div class="border-b pt-4 pb-1"> Dialogflow (Google)</div>
+        <div class="border-b pt-4 pb-1"> Lex (AWS)</div>
+        <div class="border-b pt-4 pb-1"> Qna (Microsoft) </div>
+        <div class="border-b pt-4 pb-1"> Web / Mobile Chatbot</div>
+        <div class="border-b pt-4 pb-1">Soporte  <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Widgets/website<p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1">Analytics (prox.) <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Colaboración  <span  class='uppercase pt-1 font-bold'> X</span></div>
+        <button class='border-2 mt-4 mb-8 px-6 py-4 uppercase border-black' onClick={() => this.co()}>Subscribe</button>
       </div>
-      <div class='flex-1  text-gray-700 text-center  p-2 m-1  rounded-lg shadow-md'>
-        Standar
-        <div> USD {this.calculatePrice(this.plans.standar)}</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <button onClick={() => this.co()}>Subscribe</button>
+      <div class='flex-1  text-gray-700 text-center box2'>
+        <p class='text-4xl plan_name  pt-4'>Standar</p>
+        <div class='price text-4xl pt-4'> $ {this.calculatePrice(this.plans.standar)}</div>
+        <div class="border-b pt-4"> por mes</div>
+        <div class="border-b pt-4 pb-1"> <p>Periodo de pruebas</p><p  class='uppercase pt-1 font-bold'>Ilimitado</p></div>
+        <div  class="border-b pt-4 pb-1" > Mensajes/Mes <span  class='uppercase pt-1 font-bold'> 500 </span></div>
+        <div class="border-b pt-4 pb-1"> Chatbot Widgets <p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1"> Watson Assistant (IBM)  </div>
+        <div class="border-b pt-4 pb-1"> Dialogflow (Google)</div>
+        <div class="border-b pt-4 pb-1"> Lex (AWS)</div>
+        <div class="border-b pt-4 pb-1"> Qna (Microsoft) </div>
+        <div class="border-b pt-4 pb-1"> Web / Mobile Chatbot</div>
+        <div class="border-b pt-4 pb-1">Soporte  <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Widgets/website<p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1">Analytics (prox.) <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Colaboración  <span  class='uppercase pt-1 font-bold'> X</span></div>
+        <button class='border-2 mt-4 mb-8 px-6 py-4 uppercase border-black' onClick={() => this.co()}>Subscribe</button>
       </div>
-      <div class='flex-1 text-gray-700 text-center  p-2 m-1  rounded-lg shadow-md'>
-        Advance
-        <div> USD {this.calculatePrice(this.plans.advance)}</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <button>Subscribe</button>
+      <div class='flex-1 text-gray-700 text-center box3 '>
+        <p class='text-4xl plan_name  pt-4'>Advance</p>
+        <div class='text-4xl price pt-4'> $ {this.calculatePrice(this.plans.advance)}</div>
+        <div  class="border-b pt-4"> por mes</div>
+        <div class="border-b pt-4 pb-1"> <p>Periodo de pruebas</p><p  class='uppercase pt-1 font-bold'>Ilimitado</p></div>
+        <div  class="border-b pt-4 pb-1" > Mensajes/Mes <span  class='uppercase pt-1 font-bold'> 500 </span></div>
+        <div class="border-b pt-4 pb-1"> Chatbot Widgets <p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1"> Watson Assistant (IBM)  </div>
+        <div class="border-b pt-4 pb-1"> Dialogflow (Google)</div>
+        <div class="border-b pt-4 pb-1"> Lex (AWS)</div>
+        <div class="border-b pt-4 pb-1"> Qna (Microsoft) </div>
+        <div class="border-b pt-4 pb-1"> Web / Mobile Chatbot</div>
+        <div class="border-b pt-4 pb-1">Soporte  <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Widgets/website<p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1">Analytics (prox.) <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Colaboración  <span  class='uppercase pt-1 font-bold'> X</span></div>
+        <button class='border-2 mt-4 mb-8 px-6 py-4 uppercase border-black' onClick={() => this.co()}>Subscribe</button>
       </div>
-      <div class='flex-1 text-gray-700 text-center  p-2 m-1  rounded-lg shadow-md'>
-        Premium
-        <div> USD {this.calculatePrice(this.plans.premium)}</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <div> SI</div>
-        <button>Subscribe</button>
+      <div class='flex-1 text-gray-700 text-center box4'>
+        <p class='text-4xl plan_name  pt-4'>Premium</p>
+        <div  class='text-4xl price pt-4'> $ {this.calculatePrice(this.plans.premium)}</div>
+        <div class="border-b pt-4"> por mes</div>
+        <div class="border-b pt-4 pb-1"> <p>Periodo de pruebas</p><p  class='uppercase pt-1 font-bold'>Ilimitado</p></div>
+        <div  class="border-b pt-4 pb-1" > Mensajes/Mes <span  class='uppercase pt-1 font-bold'> 500 </span></div>
+        <div class="border-b pt-4 pb-1"> Chatbot Widgets <p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1"> Watson Assistant (IBM)  </div>
+        <div class="border-b pt-4 pb-1"> Dialogflow (Google)</div>
+        <div class="border-b pt-4 pb-1"> Lex (AWS)</div>
+        <div class="border-b pt-4 pb-1"> Qna (Microsoft) </div>
+        <div class="border-b pt-4 pb-1"> Web / Mobile Chatbot</div>
+        <div class="border-b pt-4 pb-1">Soporte  <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Widgets/website<p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1">Analytics (prox.) <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Colaboración  <span  class='uppercase pt-1 font-bold'> X</span></div>
+        <button class='border-2 mt-4 mb-8 px-6 py-4 uppercase border-black' onClick={() => this.co()}>Subscribe</button>
+      </div>
+      <div class='flex-1 text-gray-700 text-center box5'>
+        <p class='text-4xl plan_name  pt-4'>Enterprice</p>
+        <div  class='text-4xl price pt-4'> CUSTOM</div>
+        <div class="border-b pt-4"> por mes</div>
+        <div class="border-b pt-4 pb-1"> <p>Periodo de pruebas</p><p  class='uppercase pt-1 font-bold'>Ilimitado</p></div>
+        <div  class="border-b pt-4 pb-1" > Mensajes/Mes <span  class='uppercase pt-1 font-bold'> 500 </span></div>
+        <div class="border-b pt-4 pb-1"> Chatbot Widgets <p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1"> Watson Assistant (IBM)  </div>
+        <div class="border-b pt-4 pb-1"> Dialogflow (Google)</div>
+        <div class="border-b pt-4 pb-1"> Lex (AWS)</div>
+        <div class="border-b pt-4 pb-1"> Qna (Microsoft) </div>
+        <div class="border-b pt-4 pb-1"> Web / Mobile Chatbot</div>
+        <div class="border-b pt-4 pb-1">Soporte  <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Widgets/website<p class='uppercase pt-1 font-bold'>ilimitado</p></div>
+        <div class="border-b pt-4 pb-1">Analytics (prox.) <span  class='uppercase pt-1 font-bold'> BÁSICO</span></div>
+        <div class="border-b pt-4 pb-1">Colaboración  <span  class='uppercase pt-1 font-bold'> X</span></div>
+        <button class='border-2 mt-4 mb-8 px-6 py-4 uppercase border-black' onClick={() => this.co()}>Subscribe</button>
       </div>
     </div>
   }
